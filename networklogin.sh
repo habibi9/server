4c50604bec461aa27f57e1daca9c6c9bda83c7ca
habibi9 <mrj17@mrj17.mails.tsinghua.edu.cn>
2017-10-28 22:14:28 +0800

    added

---
+++ networklogin.sh
@@ -0,0 +1,1 @@
+curl -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data 'action=login&username=mrj17&password={MD5_HEX}e76fbb772f88441be64c77617a706e48&ac_id=1' http://166.111.204.120/do_login.php
